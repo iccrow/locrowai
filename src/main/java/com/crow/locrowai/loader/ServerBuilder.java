25c2140fb09966ca1f1a5461c4150cc33bef91c7
iccrow <155780488+iccrow@users.noreply.github.com>
2025-09-21 14:11:52 -0400

    LocrowAI 0.2.2: Fixed an issue where the PATH env was not passed to the server startup process.

--- src/main/java/com/crow/locrowai/loader/ServerBuilder.java
+++ src/main/java/com/crow/locrowai/loader/ServerBuilder.java
@@ -1,6 +1,5 @@
 package com.crow.locrowai.loader;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.Locale;
 
@@ -8,9 +7,9 @@ public class ServerBuilder {
 
     public static ProcessBuilder builder(SystemProbe.ProbeResult si, Path dir) {
         ProcessBuilder processBuilder = new ProcessBuilder();
-        Path pyexe = dir.resolve("venv/Scripts/python.exe").normalize();
+        Path pybat = dir.resolve("python.bat").normalize();
         if (si.os().name().toLowerCase(Locale.ROOT).contains("win")) {
-            processBuilder.command(pyexe.toString(), "-m", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000");
+            processBuilder.command("cmd.exe", "/c", pybat.toString(), "-m", "uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000");
         }
         processBuilder.directory(dir.toFile());
 
